1bccc072edf1894daf33f969bc9dfb461ff9c6d0
Richard Wigley <Richard.Wigley@gmail.com>
2013-12-14 22:30:06 +0000

    NUCLEOTIDE count added

---
+++ ruby/nucleotide-count/dna.rb
@@ -0,0 +1,39 @@
+require 'pry'
+
+
+class DNA
+  def initialize strand
+    validate_arguments(strand)
+    @nucleotides = strandify strand
+  end
+
+  def count match_nucleotide
+    validate_arguments match_nucleotide
+    @nucleotides.fetch(match_nucleotide, 0)
+  end
+
+  def nucleotide_counts
+    {
+    'A' => @nucleotides.fetch('A',0),
+    'T' => @nucleotides.fetch('T',0),
+    'C' => @nucleotides.fetch('C',0),
+    'G' => @nucleotides.fetch('G',0),
+    }
+  end
+
+  private
+
+  def validate_arguments strand
+    raise(ArgumentError, 'Strands should only contain ACGT') unless nucleic_sequence(strand)
+  end
+
+  def strandify strand
+    strand.chars.each_with_object(Hash.new(0)) do |nucleotide, hsh|
+      hsh[nucleotide] += 1
+    end
+  end
+
+  def nucleic_sequence sequence
+    sequence.scan(/[ACGT]/).count == sequence.size
+  end
+end
