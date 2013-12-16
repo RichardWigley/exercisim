f814a4459aa98d46c14a0987b8c3c0731c14441b
Richard Wigley <Richard.Wigley@gmail.com>
2013-12-16 09:55:36 +0000

    Phone-number removed magic numbers

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,13 +1,15 @@
 class PhoneNumber
   attr_reader :number
   INVALID_NUMBER = '0000000000'
+  DIGITS_IN_LOCAL = 10
+  DIGITS_IN_INTERNATIONAL = 11
 
   def initialize number
     @number = clean_number(number)
   end
 
   def area_code
-    @number[0..2]
+    number[0..2]
   end
 
   def to_s
@@ -18,30 +20,30 @@ class PhoneNumber
 
   def clean_number dirty_number
     return INVALID_NUMBER unless valid?(to_digits dirty_number)
-    to_digits(dirty_number).chars.last(10).join
+    to_digits(dirty_number).chars.last(DIGITS_IN_LOCAL).join
   end
 
   def valid? number
     local?(number) || international?(number)
   end
 
-  def local? number
-    number.size == 10
-  end
-
-  def international? number
-    number.size == 11 && number.start_with?('1')
-  end
-
   def to_digits number
     number.scan(/\d+/).join
   end
 
+  def local? number
+    number.size == DIGITS_IN_LOCAL
+  end
+
+  def international? number
+    number.size == DIGITS_IN_INTERNATIONAL && number.start_with?('1')
+  end
+
   def prefix
-    @number[3..5]
+    number[3..5]
   end
 
   def identifier
-    @number[6..9]
+    number[6..9]
   end
 end
